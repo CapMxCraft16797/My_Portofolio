2ac61b5531bb9b5ea7df575c7ccd8d970f3d7981
CapMxCraft16797 <mostafaatef16797@gmail.com>
2024-08-11 15:12:53 +0300

    responsive change

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,11 +5,11 @@ import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
-    <nav className=" mb-10 flex items-center justify-between py-6   ">
-      <div className="flex flex-shrink-0 items-center">
-        <img className="h-5 w-20" src={logo} alt="logo" />
+    <nav className="sticky top-0 mb-10 flex flex-wrap items-center justify-center py-1 w-100 bg-slate-800 lg:justify-between   ">
+      <div className="flex flex-shrink-0">
+        <img className="h-5 w-20 m-5" src={logo} alt="logo" />
       </div>
-      <div className=" flex gap-5 bg-gradient-to-r  from-pink-500 via-slate-500 to-purple-500 bg-clip-text text-xl tracking-tight text-transparent">
+      <div className=" flex flex-wrap  gap-5 bg-gradient-to-r  from-pink-500 via-slate-500 to-purple-500 bg-clip-text text-xl tracking-tight text-transparent">
         <Link
           to="about"
           smooth={true}
@@ -51,7 +51,7 @@ const Navbar = () => {
           Contacts
         </Link>
       </div>
-      <div className="flex items-center justify-center gap-4 m-8 text-2xl">
+      <div className="flex flex-wrap items-center justify-center gap-4 m-8 text-2xl ">
         <a href="https://github.com/CapMxCraft16797">
           <FaGithub className="hover:text-neutral-500 cursor-pointer" />
         </a>
@@ -64,7 +64,7 @@ const Navbar = () => {
         <a href="https://www.facebook.com/profile.php?id=100008395536966">
           <FaFacebook className="hover:text-neutral-500 cursor-pointer" />
         </a>
-        <a href="https://www.instagram.com/capmxqwerty/">
+        <a className="mr-5" href="https://www.instagram.com/capmxqwerty/">
           <FaInstagram className="hover:text-neutral-500 cursor-pointer" />
         </a>
       </div>
